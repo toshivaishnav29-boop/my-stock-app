fdfdfc693cd88004120aa0f86915a47dd2932cc5
toshivaishnav29-boop <toshi.vaishnav29@gmail.com>
2026-05-10 20:50:31 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-import yfinance as df
+import yfinance as yf
 import pandas as pd
 
 st.title("Nifty 50 Tracker")
-st.write("Live Data from Yahoo Finance")
 
+# Nifty ka data download karna
 ticker = "^NSEI"
-data = df.download(ticker, period="1d", interval="1m")
+data = yf.download(ticker, period="1d", interval="1m")
 
 if not data.empty:
     current_price = data['Close'].iloc[-1]
-    st.metric(label="Nifty 50", value=f"{current_price:,.2f}")
+    st.metric(label="Nifty 50 Live Price", value=f"INR {current_price:,.2f}")
     st.line_chart(data['Close'])
 else:
-    st.write("Data loading... please wait.")
+    st.write("Data load nahi ho raha hai, market hours check karein.")
